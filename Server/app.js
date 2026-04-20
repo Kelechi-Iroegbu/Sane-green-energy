2350fe378e830153e65b2de07a52277e5854dd1d
Kelly <74309549+Kelechi-Iroegbu@users.noreply.github.com>
2026-04-20 18:36:25 +0100

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -32,7 +32,7 @@ app.use("/api/orders", orderRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
